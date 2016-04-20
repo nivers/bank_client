f607a80a326dbf7ecc62ab509327a8e3b594be62
Nicholas Ivers <nicholas.k.ivers@gmail.com>
2016-04-20 19:19:13 -0400

    initial commit; includes setup for webpack dev server

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bank Client
+
+Toy banking app built with React/Redux
+
+###Getting Started###
+
+To run the application locally, clone the repo, navigate to its top directory, and run "npm start" to run the webpack dev server. The app is configured to use port 8080.
+
+```
+	> git clone git@github.com:nivers/bank_client.git
+	> cd bank_client
+	> npm install
+	> npm start
+```
---
+++ test/helpers/mock_dom.js
@@ -0,0 +1,26 @@
+//npm
+import chai, { expect } from 'chai';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+
+import jsdom from 'jsdom';
+const mockDock = ('<!doctype html><html><body></body></html>');
+global.document = jsdom.jsdom(mockDock);
+global.window = global.document.defaultView;
+
+import jquery from 'jquery';
+const $ = jquery(global.window);
+
+import chaiJquery from 'chai-jquery';
+chaiJquery(chai, chai.util, $);
+
+function renderComponent(ComponentClass, props) {
+  const componentInstance = TestUtils.renderIntoDocument(
+    <ComponentClass {...props} />
+  );
+
+  return $(ReactDOM.findDOMNode(componentInstance));
+}
+
+export default renderComponent;
