ad3877bc8272d5bc49a8b771b4b111b27dbcfb8e
Nicholas Ivers <nicholas.k.ivers@gmail.com>
2016-04-21 19:03:28 -0400

    added action types files, and stub action function in actions index

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import { DEPOSIT_FUNDS, WITHDRAW_FUNDS } from './types';
+
+
+export function depositFunds(amount) {
+
+}
+
+export function withdrawFunds(amount) {
+
+}
---
+++ src/actions/types.js
@@ -0,0 +1,2 @@
+export const DEPOSIT_FUNDS = 'deposit_funds';
+export const WITHDRAW_FUNDS = 'withdraw_funds';
