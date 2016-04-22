d9c8075ea16046a4b4e45e0978dfd1e94327f327
Nicholas Ivers <nicholas.k.ivers@gmail.com>
2016-04-22 19:53:17 -0400

    wired up app store with validate transaction middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+//node modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+//local
 import App from './components/app';
 import reducers from './reducers';
+import validateTransaction from './middleware/validate_transaction';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(validateTransaction)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
