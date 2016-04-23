5d752f1eb42d1968fd0145f5c666aa350bd13c37
Nicholas Ivers <nicholas.k.ivers@gmail.com>
2016-04-23 15:22:01 -0400

    added manage account route and linked it to manage account container

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,13 @@ import { Route, IndexRoute } from 'react-router';
 //local
 import App from './components/app';
 import LandingPage from './components/landing_page';
+import ManageAccount from './containers/manage_account';
+
+export const MANAGE_ACCOUNT_ROUTE = '/manage_account';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={LandingPage} />
+    <Route path={MANAGE_ACCOUNT_ROUTE} component={ManageAccount} />
   </Route>
 );
