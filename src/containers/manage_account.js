81762716e2946e9d25d0e85538ed713ec0c7a34a
Nicholas Ivers <nicholas.k.ivers@gmail.com>
2016-04-23 15:19:53 -0400

    fleshed out manage account container stub

---
+++ src/containers/manage_account.js
@@ -0,0 +1,10 @@
+//node modules
+import React, { Component } from 'react';
+
+export default class ManageAccount extends Component {
+  render() {
+    <div>
+      This was rendered in the manage account container
+    </div>
+  }
+}
