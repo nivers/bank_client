58efdab4e3e5344bc5def522ca0cf2a48047e6d8
Nicholas Ivers <nicholas.k.ivers@gmail.com>
2016-04-23 15:51:32 -0400

    added basic landing page with a linkg to manage account page

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -1,10 +1,29 @@
 //node modules
 import React from 'react';
+import { Link } from 'react-router';
+
+//local
+import { MANAGE_ACCOUNT_ROUTE } from '../routes';
 
 export default function() {
   return (
-    <div>
-      This is the landing page
+    <div className="landing-page">
+
+      <div className="ui row">
+        <h1>
+          Welcome to Iron Bank Of Braavos - Online Banking
+        </h1>
+        <h2>
+          Valar Financus
+        </h2>
+      </div>
+
+      <div className="ui row">
+        <Link to={MANAGE_ACCOUNT_ROUTE}>
+          Manage Account
+        </Link>
+      </div>
+
     </div>
   );
 }
